95234c6070021c3421b900e3b0df493f9ab2c6a1
Rahul <rahuldeeti.z@gmail.com>
2025-03-18 13:11:07 +0530

    revert: Revert "test(employee-service): unit tests for EmployeeServiceImpl"
    This reverts commit b61823c58cdbc6124538a2ace6a7cfed6b494a05.

--- src/main/java/com/rahul/ems/service/EmployeeServiceImpl.java
+++ src/main/java/com/rahul/ems/service/EmployeeServiceImpl.java
@@ -43,8 +43,6 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     Employee dbEmployee = employeeRepository.save(newEmployee);
 
-//    System.out.println(dbEmployee.getFirstName() + "  firstname=============================");
-
     return employeeMapper.toDto(dbEmployee);
   }
 
