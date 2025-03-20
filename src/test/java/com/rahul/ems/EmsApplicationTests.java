e976052e22cb52b2667f6ed49dbd7f50f2c9a6eb
Rahul <rahuldeeti.z@gmail.com>
2025-03-20 10:22:16 +0530

    chore: workflow fix, set test profile for tests

--- src/test/java/com/rahul/ems/EmsApplicationTests.java
+++ src/test/java/com/rahul/ems/EmsApplicationTests.java
@@ -2,10 +2,14 @@ package com.rahul.ems;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class EmsApplicationTests {
 
   @Test
-  void contextLoads() {}
+  void contextLoads() {
+    // This is init test
+  }
 }
